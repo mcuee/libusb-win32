e4b7b825b599554d7dca38bfdedcdbf11d5c6346
ste_meyer <ste_meyer@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2005-01-05 14:04:53 +0000

    files added/removed

---
+++ libusb/src/driver/power.c
@@ -0,0 +1,28 @@
+/* LIBUSB-WIN32, Generic Windows USB Driver
+ * Copyright (C) 2002-2004 Stephan Meyer, <ste_meyer@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include "libusb_driver.h"
+
+
+NTSTATUS dispatch_power(libusb_device_extension *device_extension, IRP *irp)
+{
+  PoStartNextPowerIrp(irp);
+  IoSkipCurrentIrpStackLocation(irp);
+  return PoCallDriver(device_extension->next_stack_device, irp);
+}
