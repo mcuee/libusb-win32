ea7e95dced8a0b1740cb9831a3710cb727c381de
ste_meyer <ste_meyer@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2005-11-10 20:15:22 +0000

    file created

---
+++ trunk/libusb/ddk_make/make_test.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set TESTS_DIR=..\tests
+set SRC_DIR=..\src
+
+copy sources_test sources
+copy %TESTS_DIR%\testlibusb.c .
+copy %SRC_DIR%\usb.h .
+copy %SRC_DIR%\*.rc .
+
+build
+
+call make_clean.bat
