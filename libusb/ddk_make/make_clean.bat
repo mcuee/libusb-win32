82f051214234584b212d3ec4bb22352b48b1798f
ste_meyer <ste_meyer@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2006-03-18 13:46:09 +0000

    added x64 support

--- libusb/ddk_make/make_clean.bat
+++ libusb/ddk_make/make_clean.bat
@@ -1,39 +1,36 @@
 @echo off
 
-if exist objchk_wxp_x86\i386\*.exe (
-  copy /y objchk_wxp_x86\i386\*.exe .
+set OUTDIR=
+
+if exist output\i386 (
+  set OUTDIR=output\i386
+)
+if exist output\amd64 (
+  set OUTDIR=output\amd64
+)
+if exist output\ia64 (
+  set OUTDIR=output\ia64
 )
 
-if exist objfre_wxp_x86\i386\*.exe (
-  copy /y objfre_wxp_x86\i386\*.exe .
+
+if exist %OUTDIR%\*.exe (
+  copy /y %OUTDIR%\*.exe .
+)
+if exist %OUTDIR%\*.dll (
+  copy /y %OUTDIR%\*.dll .
+)
+if exist %OUTDIR%\*.lib (
+  copy /y %OUTDIR%\*.lib .
+)
+if exist %OUTDIR%\*.sys (
+  copy /y %OUTDIR%\*.sys .
 )
 
-if exist objchk_wxp_x86\i386\*.dll (
-  copy /y objchk_wxp_x86\i386\*.dll .
-)
-
-if exist objfre_wxp_x86\i386\*.dll (
-  copy /y objfre_wxp_x86\i386\*.dll .
-)
-
-if exist objchk_wxp_x86\i386\*.lib (
-  copy /y objchk_wxp_x86\i386\*.lib .
-)
-
-if exist objfre_wxp_x86\i386\*.lib (
-  copy /y objfre_wxp_x86\i386\*.lib .
-)
-
-if exist objchk_wxp_x86\i386\*.sys (
-  copy /y objchk_wxp_x86\i386\*.sys .
-)
-
-if exist objfre_wxp_x86\i386\*.sys (
-  copy /y objfre_wxp_x86\i386\*.sys .
-)
-
+if exist output rmdir /s /q output
 if exist objchk_wxp_x86 rmdir /s /q objchk_wxp_x86
-if exist objfre_wxp_x86 rmdir /s /q objfre_wxp_x86
+if exist objchk_wnet_AMD64 rmdir /s /q objchk_wnet_AMD64
+if exist objchk_wnet_IA64 rmdir /s /q objchk_wnet_IA64
+
 if exist sources del /q sources 
 if exist *.def del *.def 
 if exist *.h del *.h 
