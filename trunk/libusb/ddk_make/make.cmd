31c370b91350ed45ab84cdab46c2c9f15adf3d98
trobinso <trobinso@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2010-05-06 04:36:17 +0000

    Fixed 64bit inf-wizard, testlibusb-win builds

--- trunk/libusb/ddk_make/make.cmd
+++ trunk/libusb/ddk_make/make.cmd
@@ -137,6 +137,9 @@ GOTO :EOF
 	title !_title!
 	CALL make_clean.bat
 	
+	SET CMDVAR_BUILDARCH=!_BUILDARCH!
+	CALL :TagEnv ..\manifest.txt.in ..\manifest.txt
+	pause
 	CALL make_!_LIBUSB_APP!.bat
 
 	IF !ERRORLEVEL! NEQ 0 SET BUILD_ERRORLEVEL=!ERRORLEVEL!
