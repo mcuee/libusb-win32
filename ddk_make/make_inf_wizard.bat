bc3794b5f43fa004371ccc56a7d1a5cf5445126a
trobinso <trobinso@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2010-07-18 06:09:58 +0000

    Added libwdi to inf-wizard (embedded windows driver installer) Fixed inf file issue with inf2cat.exe Fixed inf issue with usb_install_driver_np()

--- ddk_make/make_inf_wizard.bat
+++ ddk_make/make_inf_wizard.bat
@@ -1,27 +1,78 @@
-@echo off
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-set TESTS_DIR=..\tests
-set SRC_DIR=..\src
-
+SET TESTS_DIR=..\tests
+SET SRC_DIR=..\src
 call make_clean.bat
 
+:: Check arguments
+::
+SET _ARGS_=%*
+IF "!LIBWDI_DIR!" EQU "" (
+	IF "%~1" EQU "" (
+		
+		ECHO The libwdi directory must be passed as first argument or the LIBWDI_DIR
+		ECHO environment variable must be set.
+		ECHO Example:
+		ECHO make_inf_wizard.bat "Z:\PROJECTS\libusb-win32-stage\projects\additional\libwdi"
+		GOTO :SHOW_LIBWDI_HELP
+	) ELSE (
+		SET LIBWDI_DIR=%~1
+		SET _ARGS_="%~2"
+	)
+)
+
+:: Check for libwdi ddk_build.cmd
+::
+IF NOT EXIST "!LIBWDI_DIR!\ddk_build.cmd" (
+	ECHO libwdi ddk_build.cmd not found at '!LIBWDI_DIR!'
+	GOTO :SHOW_LIBWDI_HELP
+)
+
+:: Build libwdi
+::
+SET BUILD_ERRORLEVEL=0
+SET _CD_=!CD!
+PUSHD !_CD_!
+
+CD /D "!LIBWDI_DIR!"
+IF EXIST "build%BUILD_ALT_DIR%.err" DEL /Q "build%BUILD_ALT_DIR%.err" >NUL
+IF EXIST "build%BUILD_ALT_DIR%.wrn" DEL /Q "build%BUILD_ALT_DIR%.wrn" >NUL
+
+IF EXIST "!LIBUSB0_DIR!" SET C_DEFINES=/DLIBUSB0_DIR=\"!LIBUSB0_DIR!\"
+CALL ddk_build.cmd static libwdi
+
+IF EXIST "build%BUILD_ALT_DIR%.err" SET BUILD_ERRORLEVEL=1
+IF EXIST "build%BUILD_ALT_DIR%.err" SET ERRORLEVEL=1
+IF !BUILD_ERRORLEVEL! NEQ 0 (
+
+	ECHO Failed building libwdi.
+	GOTO BUILD_ERROR
+)
+
+POPD
+
+copy /Y "!LIBWDI_DIR!\libwdi\libwdi.lib" >NUL
+copy /Y "!LIBWDI_DIR!\libwdi\libwdi.h" >NUL
 copy /Y sources_inf_wizard sources >NUL
-copy /Y %SRC_DIR%\*.c . >NUL
-copy /Y %SRC_DIR%\*.h . >NUL
-copy /Y %SRC_DIR%\*.rc . >NUL
-copy /Y %SRC_DIR%\libusb-win32.inf.in . >NUL
-copy /Y %SRC_DIR%\driver\driver_api.h . >NUL
-copy /Y ..\manifest_%_BUILDARCH%.xml . >NUL
+copy /Y %SRC_DIR%\inf_wizard*.* >NUL
+copy /Y %SRC_DIR%\libusb_version.h >NUL
+copy /Y %SRC_DIR%\common*.* >NUL
 
 ECHO Building (%BUILD_ALT_DIR%) %0..
-CALL build_ddk.bat %*
+CALL build_ddk.bat !_ARGS_!
 IF %BUILD_ERRORLEVEL%==0 GOTO BUILD_SUCCESS
 GOTO BUILD_ERROR
 
 :BUILD_ERROR
 ECHO [%0] WinDDK build failed (%BUILD_ALT_DIR%)
 EXIT /B 1
+GOTO BUILD_DONE
+
+:SHOW_LIBWDI_HELP
+GOTO BUILD_DONE
 
 :BUILD_SUCCESS
+GOTO BUILD_DONE
 
 :BUILD_DONE
--- ddk_make/make_super_clean.bat
+++ ddk_make/make_super_clean.bat
@@ -19,3 +19,33 @@ if exist libusb.lib del /q libusb.lib
 if exist *.cer del /q *.cer
 if exist libusb-win32.inf.in del /q libusb-win32.inf.in
 
+IF NOT EXIST ..\projects\ GOTO DONE
+
+PUSHD !CD!
+CD ..\projects
+RMDIR /S /Q .\Win32 2>NUL>NUL
+RMDIR /S /Q .\x64 2>NUL>NUL
+
+DEL /S /Q *.gitignore *.log  *.user *.ncb 2>NUL>NUL
+DEL /S /Q /AH *.suo 2>NUL>NUL
+DEL /S /Q .\additional\libwdi\*.exe 2>NUL>NUL
+DEL /S /Q .\additional\libwdi\*.lib 2>NUL>NUL
+
+RMDIR /S /Q .\additional\libwdi\libwdi\objfre_wxp_x86 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\libwdi\objchk_wxp_x86 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\libwdi\objfre_wxp_amd64 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\libwdi\objchk_wxp_amd64 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\Win32 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\x64 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\examples\objfre_wxp_x86 2>NUL>NUL
+RMDIR /S /Q .\additional\libwdi\examples\objchk_wxp_x86 2>NUL>NUL
+
+DEL .\additional\libwdi\libwdi\embedded.h 2>NUL>NUL
+
+RMDIR /S /Q ".\Debug" 2>NUL>NUL
+RMDIR /S /Q ".\Release" 2>NUL>NUL
+POPD
+
+:DONE
+
+
