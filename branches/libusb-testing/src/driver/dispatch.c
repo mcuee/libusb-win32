fa5ab071148da2082f280225a26f30cd45785dcd
trobinso <trobinso@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2010-07-28 19:32:29 +0000

    version increment removed r296 dispatch.c patch

--- branches/libusb-testing/src/driver/dispatch.c
+++ branches/libusb-testing/src/driver/dispatch.c
@@ -60,7 +60,10 @@ NTSTATUS DDKAPI dispatch(DEVICE_OBJECT *device_object, IRP *irp)
 
             if (dev->is_started)
             {
-                if (dev->power_state.DeviceState != PowerDeviceD0 && !dev->is_filter)
+				// TODO: a filter driver cannot act as the power policy owner.
+				// power_set_device_state() can only be issued by the PPO.
+				//
+                if (dev->power_state.DeviceState != PowerDeviceD0 /* && !dev->is_filter */)
                 {
                     /* power up the device, block until the call */
                     /* completes */
