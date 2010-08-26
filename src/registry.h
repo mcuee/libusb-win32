d0348da922790e53936917029c09892378c1137b
trobinso <trobinso@f7f0cd72-5b33-40cf-9e32-d63a49042dcc>
2010-08-26 23:16:48 +0000

    fixed formatting inconsistencies.

--- src/registry.h
+++ src/registry.h
@@ -1,21 +1,21 @@
 /* libusb-win32, Generic Windows USB Library
- * Copyright (c) 2002-2005 Stephan Meyer <ste_meyer@web.de>
- * Copyright (c) 2010 Travis Robinson <libusbdotnet@gmail.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+* Copyright (c) 2002-2005 Stephan Meyer <ste_meyer@web.de>
+* Copyright (c) 2010 Travis Robinson <libusbdotnet@gmail.com>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 
 
@@ -50,7 +50,7 @@ struct _filter_file_t
 typedef int filter_mode_e;
 enum _filter_mode_e
 {
-    FM_NONE    = 0,
+	FM_NONE    = 0,
 	FM_LIST    = 1 << 0,
 	FM_INSTALL = 1 << 1,
 	FM_REMOVE  = 1 << 2,
@@ -59,11 +59,11 @@ enum _filter_mode_e
 typedef int filter_type_e;
 enum _filter_type_e
 {
-    FT_NONE              = 0,
-    FT_CLASS_UPPERFILTER = 1 << 0,
-    FT_CLASS_LOWERFILTER = 1 << 1,
-    FT_DEVICE_UPPERFILTER = 1 << 2,
-    FT_DEVICE_LOWERFILTER = 1 << 3,
+	FT_NONE              = 0,
+	FT_CLASS_UPPERFILTER = 1 << 0,
+	FT_CLASS_LOWERFILTER = 1 << 1,
+	FT_DEVICE_UPPERFILTER = 1 << 2,
+	FT_DEVICE_LOWERFILTER = 1 << 3,
 };
 
 typedef struct _filter_device_t filter_device_t;
@@ -73,59 +73,59 @@ struct _filter_device_t
 
 	char device_name[MAX_PATH];
 	char device_hwid[MAX_PATH];
-    char device_mfg[MAX_PATH];
-    filter_type_e filter_type;
+	char device_mfg[MAX_PATH];
+	filter_type_e filter_type;
 };
 
 typedef struct _filter_class_t  filter_class_t;
 struct _filter_class_t
 {
 	filter_class_t* next;
-    
-    char name[MAX_PATH];
+
+	char name[MAX_PATH];
 	char class_name[MAX_PATH];
 	char class_guid[MAX_PATH];
 
-    filter_device_t* class_filter_devices;
+	filter_device_t* class_filter_devices;
 };
 
 typedef struct _filter_context_t filter_context_t;
 struct _filter_context_t
 {
-    union
-    {
-        int switches_value;
-        struct 
-        {
-            bool_t           add_all_classes:1;
-            bool_t           add_device_classes:1;
-	        bool_t           add_default_classes:1;
-        };
-    }switches;
+	union
+	{
+		int switches_value;
+		struct 
+		{
+			bool_t           add_all_classes:1;
+			bool_t           add_device_classes:1;
+			bool_t           add_default_classes:1;
+		};
+	}switches;
 
 	filter_mode_e       filter_mode;
-    filter_mode_e       filter_mode_main;
-    filter_mode_e       active_filter_mode;
+	filter_mode_e       filter_mode_main;
+	filter_mode_e       active_filter_mode;
 	filter_class_t*     class_filters;
-    filter_device_t*    device_filters;
-    filter_file_t*      inf_files;
+	filter_device_t*    device_filters;
+	filter_file_t*      inf_files;
 };
 
 bool_t usb_registry_is_nt(void);
 
 bool_t usb_registry_restart_device(HDEVINFO dev_info,
-                                   SP_DEVINFO_DATA *dev_info_data);
+								   SP_DEVINFO_DATA *dev_info_data);
 bool_t usb_registry_stop_device(HDEVINFO dev_info,
-                                SP_DEVINFO_DATA *dev_info_data);
+								SP_DEVINFO_DATA *dev_info_data);
 bool_t usb_registry_start_device(HDEVINFO dev_info,
-                                 SP_DEVINFO_DATA *dev_info_data);
+								 SP_DEVINFO_DATA *dev_info_data);
 
 bool_t usb_registry_get_property(DWORD which, HDEVINFO dev_info,
-                                 SP_DEVINFO_DATA *dev_info_data,
-                                 char *buf, int size);
+								 SP_DEVINFO_DATA *dev_info_data,
+								 char *buf, int size);
 bool_t usb_registry_set_property(DWORD which, HDEVINFO dev_info,
-                                 SP_DEVINFO_DATA *dev_info_data,
-                                 char *buf, int size);
+								 SP_DEVINFO_DATA *dev_info_data,
+								 char *buf, int size);
 
 bool_t usb_registry_restart_all_devices(void);
 
@@ -136,9 +136,9 @@ void usb_registry_start_libusb_devices(void);
 bool_t usb_registry_match(HDEVINFO dev_info, SP_DEVINFO_DATA *dev_info_data);
 
 bool_t usb_registry_get_mz_value(const char *key, const char *value,
-                                 char *buf, int size);
+								 char *buf, int size);
 bool_t usb_registry_set_mz_value(const char *key, const char *value,
-                                 char *buf, int size);
+								 char *buf, int size);
 int usb_registry_mz_string_size(const char *src);
 char *usb_registry_mz_string_find(const char *src, const char *str, bool_t no_case);
 char *usb_registry_mz_string_find_sub(const char *src, const char *str);
@@ -150,8 +150,8 @@ bool_t usb_registry_get_hardware_id(HDEVINFO dev_info,
 									SP_DEVINFO_DATA *dev_info_data, 
 									char* max_path_buffer);
 bool_t usb_registry_is_service_libusb(HDEVINFO dev_info,
-                                      SP_DEVINFO_DATA *dev_info_data,
-                                      bool_t* is_libusb_service);
+									  SP_DEVINFO_DATA *dev_info_data,
+									  bool_t* is_libusb_service);
 
 bool_t usb_registry_insert_class_filter(filter_context_t* filter_context);
 bool_t usb_registry_remove_class_filter(filter_context_t* filter_context);
@@ -160,30 +160,30 @@ bool_t usb_registry_free_class_keys(filter_class_t **head);
 bool_t usb_registry_get_usb_class_keys(filter_context_t* filter_context, bool_t refresh_only);
 bool_t usb_registry_get_all_class_keys(filter_context_t* filter_context, bool_t refresh_only);
 bool_t usb_registry_get_device_filter_type(HDEVINFO dev_info,
-                                           SP_DEVINFO_DATA *dev_info_data,
-                                           filter_type_e* filter_type);
+										   SP_DEVINFO_DATA *dev_info_data,
+										   filter_type_e* filter_type);
 
 bool_t usb_registry_add_usb_class_key(filter_context_t* filter_context, const char* class_guid);
 bool_t usb_registry_add_filter_device_keys(filter_device_t** head,
-                                           const char* hwid,
-                                           const char* name,
-                                           const char* mfg,
-                                           filter_device_t** found);
+										   const char* hwid,
+										   const char* name,
+										   const char* mfg,
+										   filter_device_t** found);
 bool_t usb_registry_add_filter_file_keys(filter_file_t** head,
-                                         const char* name,
-                                         filter_file_t** found);
+										 const char* name,
+										 filter_file_t** found);
 
 bool_t usb_registry_lookup_class_keys_by_name(filter_class_t** head);
 bool_t usb_registry_add_class_key(filter_class_t **head,
-                                  const char *key,
-                                  const char *class_name,
-                                  const char *class_guid,
-                                  filter_class_t **found,
-                                  bool_t update_only);
+								  const char *key,
+								  const char *class_name,
+								  const char *class_guid,
+								  filter_class_t **found,
+								  bool_t update_only);
 
 bool_t usb_registry_insert_device_filters(filter_context_t* filter_context);
 bool_t usb_registry_insert_device_filter(filter_context_t* filter_context, bool_t upper, 
-                                      HDEVINFO dev_info, SP_DEVINFO_DATA *dev_info_data);
+										 HDEVINFO dev_info, SP_DEVINFO_DATA *dev_info_data);
 
 bool_t usb_registry_free_filter_devices(filter_device_t **head);
 bool_t usb_registry_free_filter_files(filter_file_t **head);
