5beec48d8019991cca3127dcd849cea4498de87b
Peter Dons Tychsen <pdt@dontech.dk>
2024-01-02 00:51:53 +0100

    Remove large-transfers, as the concept is fundamentally broken

--- libusb/src/driver/ioctl.c
+++ libusb/src/driver/ioctl.c
@@ -72,17 +72,16 @@
 // calls the transfer function and returns NTSTATUS
 #define TRANSFER_IOCTL_EXECUTE()										\
 	if (transfer_buffer_length > (ULONG)(maxTransferSize))				\
-	/* split large transfers */											\
-	return large_transfer(dev, irp,					\
+	/* capped transfer */							\
+	return transfer(dev, irp,						\
 	usbdDirection,									\
 	urbFunction,									\
 	pipe_info,										\
 	request->endpoint.packet_size,					\
-	maxTransferSize,								\
 	request->endpoint.transfer_flags,				\
 	request->endpoint.iso_start_frame_latency,		\
 	transfer_buffer_mdl,							\
-	transfer_buffer_length);						\
+	maxTransferSize);					\
 	else											\
 	/* normal transfer */							\
 	return transfer(dev, irp,						\
